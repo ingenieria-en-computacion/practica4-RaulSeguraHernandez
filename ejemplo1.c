ea61150cd722a3c7ac3b26be87b3fc60e39250ca
RaulSeguraHernandez <raul3ro.segura@gmail.com>
2025-03-06 09:46:27 -0600

    orimeros 3 archivos

--- ejemplo1.c
+++ ejemplo1.c
@@ -10,11 +10,12 @@ int main(){
         printf
         ("Vector reservado:\n\t[");
         for(cont =0; cont <num; cont++){
-            printf("\t%d", *(arreglo + cont));            
+            printf("\t%d, %p", *(arreglo + cont), (arreglo+cont));            
         }
         printf("\t]\n");
         printf("Liberando el espacio reservado\n");
-        free(arreglo);        
+        free(arreglo); 
+        arreglo = NULL;      
     }
     return 0;
 }
